e593395d910a9b369aa478cd9712563342b5410e
Hardik Patel <hardikgw@gmail.com>
2018-09-05 19:29:50 -0400

    splash dependencies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {AppComponent} from './app.component';
 import {SkillsComponent} from './skills/skills.component';
+import {SplashComponent} from './splash/splash.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: SplashComponent,
     children: [
       {
         path: 'skills',
         component: SkillsComponent
-      }
+      },
     ]
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {SkillsComponent} from './skills/skills.component';
-import { AuthComponent } from './auth/auth.component';
-import { SplashComponent } from './splash/splash.component';
-import { HeaderComponent } from './layout/header/header.component';
-import { MenuComponent } from './layout/menu/menu.component';
+import {AuthComponent} from './auth/auth.component';
+import {SplashComponent} from './splash/splash.component';
+import {HeaderComponent} from './layout/header/header.component';
+import {MenuComponent} from './layout/menu/menu.component';
+import {AppModuleIncludeMaterial} from './app.module.include.material';
 
 @NgModule({
   declarations: [
@@ -21,8 +22,11 @@ import { MenuComponent } from './layout/menu/menu.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    AppModuleIncludeMaterial
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
